bcafa03ef5ad80500a3d4bd95f4f8dc70f1acbfe
Andy Tran <transient.tran@gmail.com>
2024-08-13 19:24:01 -0400

    update steps and example code

--- src/app.js
+++ src/app.js
@@ -3,8 +3,4 @@ import axios from "axios";
 const UNIFIED_WORKSPACE_ID = process.env.UNIFIED_WORKSPACE_ID;
 const UNIFIED_API_KEY = process.env.UNIFIED_API_KEY;
 
-function injectUnifiedWidget() {
-  // code to go here
-}
-
-// The rest of the code will be added during the tutorial
+// Add your code below
