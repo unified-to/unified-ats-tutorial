dd9313c0f7be95ed8712d8571ea74e601bbc7c37
Andy Tran <transient.tran@gmail.com>
2024-08-28 22:51:35 -0400

    Add env file and rename key to token

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const UNIFIED_WORKSPACE_ID = process.env.UNIFIED_WORKSPACE_ID;
-const UNIFIED_API_KEY = process.env.UNIFIED_API_KEY;
+const UNIFIED_API_KEY = process.env.UNIFIED_API_TOKEN;
 
 // Add your code below
